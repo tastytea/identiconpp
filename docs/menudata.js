b7414579382be49d39d3c9cd52c4fd4983cbff47
tastytea <tastytea@tastytea.de>
2018-12-28 02:38:37 +0100

    2018-12-28

--- docs/menudata.js
+++ docs/menudata.js
@@ -31,4 +31,5 @@ var menudata={children:[
 {text:"Functions",url:"functions_func.html"},
 {text:"Enumerations",url:"functions_enum.html"}]}]},
 {text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"}]}]}
+{text:"File List",url:"files.html"}]},
+{text:"Examples",url:"examples.html"}]}
